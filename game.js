db58bf1a60d88540673fda14d512533ab919d283
TheBlueTophat <tbtproductionswork@gmail.com>
2021-11-28 05:55:39 -0500

    More stuff. - Shooting added, refinements still needed

--- game.js
+++ game.js
@@ -22,25 +22,20 @@ document.addEventListener("keyup", function(){keyHandler(event, false)}, false);
 
 let sfxVolume = 1; // Ratio, 1 = 100%, 0.5 = 50%, 0 = 0%
 
-let rightPressed = false;
-let leftPressed = false;
+let kD =        {up: false, down: false, left: false, right: false, shoot: false}; // Stands for Input Key Down
+let kDPrev =    {up: false, down: false, left: false, right: false, shoot: false}; // kD values from the previous frame
+let kJP =       {up: false, down: false, left: false, right: false, shoot: false}; // Stands for Key Just Pressed
 
-let KD =            {up: false, down: false, left: false, right: false, shoot: false}; // Stands for inputKeyDown
-let KDprev =        {up: false, down: false, left: false, right: false, shoot: false}; // KD values from the previous frame
-let justPressed =   {up: false, down: false, left: false, right: false, shoot: false};
-
-// Stands for KeyboardKey
-// Default uses the arrow keys to move and spacebar to shoot
-const kbKey = {up: "ArrowUp", down: "ArrowDown", left: "ArrowLeft", right: "ArrowRight", shoot: ["w", "W"]};
+const kBKey = {up: "ArrowUp", down: "ArrowDown", left: "ArrowLeft", right: "ArrowRight", shoot: ["w", "W"]}; // Stands for Key Board Key
 
 function keyHandler(event, setState)
 {
-    console.log(event.key);
+    // console.log(event.key);
 
     // I don't get why this doesn't throw an error whenever a key is pressed that isn't in the kbKey
     // What getKeyByValue returns here is "undefined" for any keys that aren't the ones already defined.
     // I don't see any error otherwise though. Oh well. It works! 
-    KD[getKeyByValue(kbKey, event.key)] = setState;
+    kD[getKeyByValue(kBKey, event.key)] = setState;
 }
 
 
@@ -78,65 +73,85 @@ function playSound(path)
 }
 
 
-// Creates the first sprite, adds it to the stage
-let sprite = PIXI.Sprite.from("random_goomba_head.png");
-sprite.width = 10;
-sprite.height = 10;
+// Shorter version of console.log();
+function p(val)
+{
+    console.log(val);
+}
 
+// Creates the first sprite, adds it to the stage
+// let sprite = PIXI.Sprite.from("random_goomba_head.png");
+let sprite = new PIXI.Graphics();
+sprite.beginFill(0x00CCAA);
+sprite.drawRect(0, 0, 10, 10)
+// sprite.width = 10;
+// sprite.height = 10;
 app.stage.addChild(sprite);
 
+let playerShots = [];
 
 // Main game loop
 let elapsed = 0.0;
-app.ticker.add((delta) => {
-elapsed += delta;
+app.ticker.add((delta) => mainLoop(delta));
 
-// ---- Start of the loop ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
-
-
-for (const key in justPressed)
+function mainLoop(delta)
 {
-    if(KDprev[key] == false && KD[key] == true)
+    elapsed += delta;
+
+    // Handles justPressed functionality (move to function?)
+    for (const key in kJP)
     {
-        if(key == "shoot") console.log("justPressed true");
-        justPressed[key] = true;
+        if(kDPrev[key] == false && kD[key] == true)
+        {
+            kJP[key] = true;
+        }
+        else
+        {
+            kJP[key] = false;
+        }  
     }
-    else
+
+
+    if(kD.right)
     {
-        justPressed[key] = false;
-    }  
+        sprite.x += 1;
+    }
+
+    if(kD.left)
+    {
+        sprite.x -= 1;
+    }
+
+    if(kD.up)
+    {
+        sprite.y -= 1;
+    }
+
+    if(kD.down)
+    {
+        sprite.y += 1;
+    }
+
+    if(kJP.shoot)
+    {
+        playSound("shootLaser.wav");
+
+        let newShot = new PIXI.Graphics();
+        newShot.beginFill(0xFF0000);
+        newShot.drawRect((sprite.x + (sprite.width - 5) / 2), sprite.y + (sprite.height - 10) / 2, 5, 10);
+        app.stage.addChild(newShot);
+        playerShots.push(newShot);
+    }
+
+
+    for (shot in playerShots)
+    {
+        playerShots[shot].y -= 10; // CHANGE TO SPEED
+    }
+
+    // Stores the current state of key presses, which will lag behind by one frame on the next frame
+    for (const key in kDPrev)
+    {
+        kDPrev[key] = kD[key];
+    }
 }
-
-if(KD.right)
-{
-    sprite.x += 1;
-}
-
-if(KD.left)
-{
-    sprite.x -= 1;
-}
-
-if(KD.up)
-{
-    sprite.y -= 1;
-}
-
-if(KD.down)
-{
-    sprite.y += 1;
-}
-
-if(justPressed.shoot)
-{
-    playSound("shootLaser.wav");
-}
-
-for (const key in KDprev)
-{
-    KDprev[key] = KD[key];
-}
-
-
-// ---- End of the loop ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
-});
