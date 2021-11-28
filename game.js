00a0b8ed44d6a8b78fb32687ce280169e13b15e8
TheBlueTophat <tbtproductionswork@gmail.com>
2021-11-28 04:51:02 -0500

    - Added aliasing for inputs! - Other misc stuff.

--- game.js
+++ game.js
@@ -5,6 +5,8 @@
 // -- https://jsfiddle.net/4j54jqt2/4
 
 // Remember, Ctrl + Shift + R to reload the page as well as refresh the cache
+
+// Initializes the stage, prints some relevant  info.
 let app = new PIXI.Application({width: 1000, height: 600});
 document.body.appendChild(app.view);
 
@@ -12,11 +14,11 @@ console.log(window.innerWidth);
 console.log(window.innerHeight);
 console.log("Test");
 
-// Initializes the input handlers
 
+// Initializes the input handlers
 document.addEventListener("keydown", function(){keyHandler(event, true)}, false);
 document.addEventListener("keyup", function(){keyHandler(event, false)}, false);
-// document.addEventListener("onkeypress", function(){keyHandler(event, true, "onkeypress")}, false);
+
 
 let sfxVolume = 1; // Ratio, 1 = 100%, 0.5 = 50%, 0 = 0%
 
@@ -29,7 +31,7 @@ let justPressed =   {up: false, down: false, left: false, right: false, shoot: f
 
 // Stands for KeyboardKey
 // Default uses the arrow keys to move and spacebar to shoot
-const kbKey = {up: "ArrowUp", down: "ArrowDown", left: "ArrowLeft", right: "ArrowRight", shoot: "w"};
+const kbKey = {up: "ArrowUp", down: "ArrowDown", left: "ArrowLeft", right: "ArrowRight", shoot: ["w", "W"]};
 
 function keyHandler(event, setState)
 {
@@ -39,13 +41,34 @@ function keyHandler(event, setState)
     // What getKeyByValue returns here is "undefined" for any keys that aren't the ones already defined.
     // I don't see any error otherwise though. Oh well. It works! 
     KD[getKeyByValue(kbKey, event.key)] = setState;
-    // console.log(getKeyByValue(kbKey, event.keyCode));
 }
 
+
+// Returns the key name from an object passed to it which equals value (or if it is an array, one of the indices is equal to value)
 function getKeyByValue(object, value)
 {
-    return Object.keys(object).find(key => object[key] === value);
+    for (key in object)
+    {
+        if(object[key] === value)
+        {
+            return key;
+        }
+        else if((typeof object[key]) === 'object')
+        {
+            for (key2 in object[key])
+            {
+                if(object[key][key2] === value)
+                {
+                    return key;
+                }
+            }
+        }
+    }
+
+    return null; // IDK what I should make this return. This seems to work though so -\('-')/-
+    // return Object.keys(object).find(key => object[key] === value); // Old code, probably more efficient but not complex enough for what I need.
 }
+
 
 function playSound(path)
 {
@@ -54,6 +77,7 @@ function playSound(path)
     audio.play();
 }
 
+
 // Creates the first sprite, adds it to the stage
 let sprite = PIXI.Sprite.from("random_goomba_head.png");
 sprite.width = 10;
@@ -61,12 +85,14 @@ sprite.height = 10;
 
 app.stage.addChild(sprite);
 
+
 // Main game loop
 let elapsed = 0.0;
 app.ticker.add((delta) => {
 elapsed += delta;
 
-// ---- Start of the loop ---- ---- ----
+// ---- Start of the loop ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
 
 for (const key in justPressed)
 {
@@ -111,5 +137,6 @@ for (const key in KDprev)
     KDprev[key] = KD[key];
 }
 
-// ---- End of the loop ---- ---- ----
+
+// ---- End of the loop ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 });
