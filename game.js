61f5c16a9216af84e5d0e3cd30b8f72bceddc4a0
TheBlueTophat <tbtproductionswork@gmail.com>
2022-01-05 19:11:06 -0500

    Tried to fix the physics, unsuccessfully.

--- game.js
+++ game.js
@@ -215,6 +215,7 @@ player.ax = 0;
 player.ay = 0;
 player.angleV = 0; // rotational velocity
 player.angleA = 0; // rotational acceleration
+player.visible = false;
 
 const PLAYER_ACCEL_DECAY = 1;
 const VEL_MAX = 2; // turning/drifting velocity maximum
@@ -233,6 +234,7 @@ radiusLine.x = 0;
 radiusLine.y = 0;
 // radiusLine.lineStyle(5, 0xffffff).moveTo(player.x, player.y).lineTo(0, 0);
 radiusLine.lineStyle(5, RADIUS_COLOR);
+radiusLine.visible = false;
 
 world.stage.addChild(radiusLine);
 
@@ -276,7 +278,7 @@ function mainLoop(delta)
 
     justPressedHandler(); 
 
-    // GOOD
+    // GOOD?
     let cx = player.x - Math.cos(player.angle) * radius;
     let cy = player.y + Math.sin(player.angle) * radius;
 
@@ -300,12 +302,12 @@ function mainLoop(delta)
 
         debugBank[0] = debugString({"cx":cx, "cy":cy});
 
-        let dot = new PIXI.Graphics();
-        dot.beginFill(0x00CC00);
-        dot.drawRect(0, 0, 1, 1);
-        dot.x = cx;
-        dot.y = cy;
-        world.stage.addChild(dot);
+        // let dot = new PIXI.Graphics();
+        // dot.beginFill(0x00CC00);
+        // dot.drawRect(0, 0, 1, 1);
+        // dot.x = cx;
+        // dot.y = cy;
+        // world.stage.addChild(dot);
 
         // DEBUG 2
         // let dot2 = new PIXI.Graphics();
@@ -318,8 +320,9 @@ function mainLoop(delta)
         // dummy.angleV += rA;
 
         // dummy.angle += dummy.angleV;
-        dummy.angleV += rA;
-        dummy.angle += dummy.angleV * delta;
+        // dummy.angleV += rA;
+        dummy.angleV = rA;
+        dummy.angle += dummy.angleV * delta; // Good?
 
         if(dummy.angle < 0)
         {
@@ -330,47 +333,68 @@ function mainLoop(delta)
             dummy.angle -= (2 * Math.PI);
         }
 
+        // *** delta is measured in seconds
+
         // dummy.vx += Math.cos(dummy.angle);
         // dummy.vy += Math.sin(dummy.angle);
         // velocity adds the derivative of the trig of the angle instead:
         
-        dummy.vx = -Math.sin(dummy.angle) * delta * dummy.angleV * 10;
-        dummy.vy = Math.cos(dummy.angle) * delta * dummy.angleV * 10;
+        //dummy.vx = -Math.sin(dummy.angle) * delta * dummy.angleV * 10;
+        //dummy.vy = Math.cos(dummy.angle) * delta * dummy.angleV * 10;
+        
+        // dummy.vx -= Math.cos(dummy.angleV)// * delta;
+        // dummy.vy += Math.sin(dummy.angleV)// * delta;
+
+        // dummy.vx = dummy.vx - (1) * Math.cos(dummy.angle) * radius * delta;
+        // dummy.vy = dummy.vy + (1) * Math.sin(dummy.angle) * radius * delta;
+
+        dummy.vx += -radius * Math.cos(dummy.angle);
+        dummy.vy += -radius * Math.sin(dummy.angle);
+
+
+        console.log(-Math.sin(dummy.angle) * delta * dummy.angleV * 10);
+        console.log(Math.cos(dummy.angle) * delta * dummy.angleV * 10);
+        console.log(((-Math.sin(dummy.angle) * delta * dummy.angleV * 10) ** 2) + ((Math.cos(dummy.angle) * delta * dummy.angleV * 10) ** 2));
     }
 
     dummy.x += dummy.vx;
     dummy.y += dummy.vy;
 
-    // dummy.angleV *= 0.9; // limits to 0 reasonably fast
-    dummy.vx *= 0.95;
-    dummy.vy *= 0.95;
+    
 
-    if(dummy.angleV > 0)
+    // dummy.angleV *= 0.9; // limits to 0 reasonably fast
+    // dummy.vx *= 0.95;
+    // dummy.vy *= 0.95;
+    if(false) // debug
     {
-        dummy.angleV -= (Math.PI / 180) * delta * 0.5; // 1/10th of a degree
+        if(dummy.angleV > 0)
+        {
+            dummy.angleV -= (Math.PI / 180) * delta * 0.5; // 1/10th of a degree
+
+            if(dummy.angleV < 0)
+            {
+                dummy.angleV = 0;
+            }
+        }
 
         if(dummy.angleV < 0)
         {
-            dummy.angleV = 0;
+            dummy.angleV += (Math.PI / 180) * delta * 0.5;// 1/10th of a degree;
+
+            if(dummy.angleV > 0)
+            {
+                dummy.angleV = 0;
+            }
         }
+    
+
+        dummy.angleV = clamp(dummy.angleV, -0.5, 0.5);
     }
-
-    if(dummy.angleV < 0)
-    {
-        dummy.angleV += (Math.PI / 180) * delta * 0.5;// 1/10th of a degree;
-
-        if(dummy.angleV > 0)
-        {
-            dummy.angleV = 0;
-        }
-    }
-
-    dummy.angleV = clamp(dummy.angleV, -0.5, 0.5);
 
     // dummy.x += Math.cos(dummy.angle) * ;
     // dummy.y += Math.sin(dummy.angle);
     
-    debugBank[1] = debugString({"dvx":dummy.vx, "dvy":dummy.vy, "drV":dummy.angleV});
+    debugBank[1] = debugString({"vx":dummy.vx, "vy":dummy.vy, "angV":dummy.angleV, "ang":dummy.angle * 180 / Math.PI});
 
     if(kD.left)
     {
@@ -389,12 +413,13 @@ function mainLoop(delta)
 
         debugBank[0] = debugString({"cx":cx, "cy":cy});
 
-        let dot = new PIXI.Graphics();
-        dot.beginFill(0x00CC00);
-        dot.drawRect(0, 0, 1, 1);
-        dot.x = cx;
-        dot.y = cy;
-        world.stage.addChild(dot);
+        // NO. MORE. DOTS!
+        // let dot = new PIXI.Graphics();
+        // dot.beginFill(0x00CC00);
+        // dot.drawRect(0, 0, 1, 1);
+        // dot.x = cx;
+        // dot.y = cy;
+        // world.stage.addChild(dot);
     }
 
     if(!kD.right && !kD.left)
@@ -405,9 +430,9 @@ function mainLoop(delta)
         }
     }
 
-    debugBank[2] = debugString({"x": player.x, "y": player.y, "radius": radius, "angle (deg)": player.angle * 180 / 3.14}, 4);
-    debugBank[3] = debugString({"vx": player.vx, "vy": player.vy, "vtot": ((player.vx ** 2 + player.vy ** 2 )**.5), "ax": player.ax, "ay": player.ay}, 4);
-    debugBank[4] = debugString({"cx":cx, "cy":cy, "cx-px":(cx - player.x), "cy-py":(cy-player.y)}, 4);
+    // debugBank[2] = debugString({"x": player.x, "y": player.y, "radius": radius, "angle (deg)": player.angle * 180 / 3.14}, 4);
+    // debugBank[3] = debugString({"vx": player.vx, "vy": player.vy, "vtot": ((player.vx ** 2 + player.vy ** 2 )**.5), "ax": player.ax, "ay": player.ay}, 4);
+    // debugBank[4] = debugString({"cx":cx, "cy":cy, "cx-px":(cx - player.x), "cy-py":(cy-player.y)}, 4);
 
     // if(player.angleA > 0)
     // {
@@ -420,7 +445,7 @@ function mainLoop(delta)
     // }
 
     player.angleV += player.angleA * delta;
-
+    
     // Bounds angular velocity
     if(player.angleV > 0)
     {
